7dc886f143a9181ae111387f900a34cdf8d9a46a
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-06-20 14:56:12 +0200

    risultato a console

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 
 console.log("JS OK")
+
+/*Dichiarazione variabili e funzione risultato a console*/ 
+
+const nome = prompt("Inserisci il tuo nome")
+console.log(nome)
+
+const cognome = prompt("Inserisci il tuo cognome")
+console.log(cognome)
+
+const colorePreferito = prompt("Inserisci il tuo colore preferito")
+console.log(colorePreferito)
+
+let risultato = nome + cognome + colorePreferito
+
+risultato = risultato + "21"
+console.log(risultato)
